53179fe685036b6e9bd64e84caa4e43079979226
Devo App <57620248+Ahmedbizo@users.noreply.github.com>
2022-06-26 03:25:17 +0200

    display table,sort and search

--- film-search/src/episode/ListEpisode.js
+++ film-search/src/episode/ListEpisode.js
@@ -1,5 +1,6 @@
-
+import { Container, Card, Row, Col, Table } from "react-bootstrap";
 import { useState, useEffect } from "react";
+import Form from "react-bootstrap/Form";
 
 
 function ListEpisode() {
@@ -51,8 +52,90 @@ function ListEpisode() {
 
 
   return (
-    <div></div>
-  )
+    <Container fluid>
+      <Row style={{ marginTop: "100px", marginBottom: "40px" }}>
+        <Col xl={2} sm={2}>
+          <Form.Select
+            aria-label="Default select example"
+            onChange={(e) => {
+              setSort(e.target.value);
+              handleSort(e);
+              setIsSelected(false);
+            }}
+          >
+            <option value="date">Date</option>
+            <option value="ep_id">Episode Id</option>
+          </Form.Select>
+        </Col>
+        <Col xl={4} sm={6}>
+          <Form.Control
+            type="text"
+            placeholder="Type To Search..."
+            value={searchName}
+            onChange={(e) => {
+              setSearchName(e.target.value);
+              handleNameChange(e);
+              setIsSelected(false);
+            }}
+          />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>episode id</th>
+                <th>title</th>
+                <th>release date</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.length ? data.map((item) => {
+                return (
+                  <tr
+                    key={item.episode_id}
+                    onClick={() => fetchOneEpisode(item)}
+                  >
+                    <td>EPISODE {item.episode_id}</td>
+                    <td>{item.title}</td>
+                    <td>{item.release_date}</td>
+                  </tr>
+                );
+              }) : (<tr><td colSpan={3} align={'center'}>No Matches Selected</td></tr>)}
+            </tbody>
+          </Table>
+        </Col>
+        <Col>
+          {isSelected && episode ? (
+            <Card
+              bg={"light"}
+              text={"dark"}
+              style={{ width: "full" }}
+              className="mb-2"
+            >
+              <Card.Header>{episode?.title}</Card.Header>
+              <Card.Body>
+                <Card.Text>{episode?.opening_crawl}</Card.Text>
+              </Card.Body>
+            </Card>
+          ) : (
+            <Card
+              bg={"light"}
+              text={"dark"}
+              style={{ width: "full" }}
+              className="mb-2"
+            >
+              <Card.Header>Movie Details</Card.Header>
+              <Card.Body>
+                <Card.Text>No Movies Selected</Card.Text>
+              </Card.Body>
+            </Card>
+          )}
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default ListEpisode;
